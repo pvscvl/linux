d986ef1ec8ab4bd61a31eeb4c63e984993b8d6ea
pvscvl <pascalschoofs@icloud.com>
2023-03-26 04:06:26 +0200

    Update wg.ps1

--- prep-software/ps/wg.ps1
+++ prep-software/ps/wg.ps1
@@ -1,7 +1,6 @@
-
 $WebClient = New-Object System.Net.WebClient
-Write-Host "WatchGuard System Manager 12.9 Update 1 wird geladen."
-$WebClient.DownloadFile("https://cdn.watchguard.com/SoftwareCenter/Files/WSM/12_9_U1/wsm_12_9_U1.exe")
-Write-Host "WatchGuard System Manager 12.9 Update 1 wurde geladen."
-Write-Host "Starte WatchGuard System Manager 12.9 Update 1 Installation"
-Start-Process -FilePath ./wsm_12_9.exe -Args "/SILENT" -Verb RunAs -Wait
+Write-Host "WSM 12.9.2 wird geladen"
+$WebClient.DownloadFile("https://cdn.watchguard.com/SoftwareCenter/Files/WSM/12_9_2/wsm_12_9_2.exe","C:\prep\wsm_12_9_2.exe")
+Write-Host "WSM 12.9.2 wurde geladen"
+#Start-Process -FilePath ./wsm_12_9.exe -Args "/SILENT" -Verb RunAs -Wait
+ 
