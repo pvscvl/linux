bd5084c450aa5043d422faf587bb51ac52e2a532
pvscvl <pascalschoofs@icloud.com>
2023-04-30 15:52:01 +0200

    .

--- postinstallv3-wip.sh
+++ postinstallv3-wip.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
     #    bash -c "$(wget -qLO - https://raw.githubusercontent.com/pvscvl/linux/main/postinstallv3-wip.sh)"
-    VERSION="v2023-04-30v19x"
+    VERSION="v2023-04-30v20"
     COL_NC='\e[0m' # 
     COL_GREEN='\e[1;32m'
     COL_RED='\e[1;31m'
