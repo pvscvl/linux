f716b6200f4016a6fd06108d3057c7f16663baca
pvscvl <pascalschoofs@icloud.com>
2023-08-29 02:21:49 +0200

    Create installzsh.sh

---
+++ installzsh.sh
@@ -0,0 +1,22 @@
+sudo apt update
+sudo apt install zsh -y
+chsh -s $(which zsh)
+cd $HOME
+mkdir .dotfiles
+cd $HOME/.dotfiles
+touch .aliases .functions .exports
+cd
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+apt install fonts-powerline -y
+apt install pip -y
+pip install powerline-shell 
+mkdir -p ~/.config/powerline-shell 
+powerline-shell --generate-config > ~/.config/powerline-shell/config.json
+cd
+echo " " > .zshrc
+curl https://raw.githubusercontent.com/pvscvl/linux/main/zshrc > .zshrc
+
+
+source ~/.zshrc
